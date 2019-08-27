da33fc35dc1a95ddb7bd94ed2812d1b3ff90013e
pranav661 <37332556+pranav661@users.noreply.github.com>
2019-08-28 00:26:13 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import sys
 
-workspace = sys.argv[1]
+print('starting execution')
 
+workspace = sys.argv[0]
 print(workspace)
 
-df1 = pd.read_excel(workspace+"\Master.xlsx",sheet_name='Sheet1')
+masterpath = workspace + "\Master.xlsx"
+print(masterpath)
+
+#df1 = pd.read_excel(workspace+"\Master.xlsx",sheet_name='Sheet1')
