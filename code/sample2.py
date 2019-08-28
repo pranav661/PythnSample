8260eaf8f0265d6f7a5592671d0ebdec76aa7d50
pranav661 <37332556+pranav661@users.noreply.github.com>
2019-08-28 13:34:31 +0530

    Add files via upload

---
+++ code/sample2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sys
+
+print('starting execution')
+print('starting execution 4')
+
+workspace = sys.argv[1]
+print(workspace)
+
+masterpath = workspace + "\Master.xlsx"
+print(masterpath)
